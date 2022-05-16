b1731952db9f8c861a90e8a6ae935905b8723ed0
Mr.An <42627702+SnowMoon-Dev@users.noreply.github.com>
2022-05-16 16:14:39 +0800

    add test

---
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/controller/UsersControllerTest2.java
@@ -0,0 +1,17 @@
+package org.apache.dolphinscheduler.api.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersControllerTest2 {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
